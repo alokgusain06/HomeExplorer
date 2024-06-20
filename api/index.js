c196acf73041321784eae438676cfb8360a4a26e
[Alok gusain06] <[alokgusain541@gamil.com]>
2024-06-20 23:10:20 +0530

    connect to Mongodb database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 const express=require('express');
 
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log('connect to Mongodb!!!');
+}).catch((err)=>{
+  console.log(err);
+});
+
+
 const app=express();
 
 app.listen(4000,()=>{
